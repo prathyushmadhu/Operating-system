b417c19b85b54fa946945f94d4be4aa586e282ac
prathyushmadhu <prathyushmadhu@gmail.com>
2023-08-06 12:09:10 +0530

    changed file content

--- sjf_srtf.c
+++ sjf_srtf.c
@@ -16,16 +16,14 @@ int main() {
         scanf("%d",&r[i].at);
         printf("ENTER BURST TIME : ");
         scanf("%d",&r[i].bt);
-
         r[i].rt = r[i].bt;
         r[i].wt = 0;
         r[i].rest = 0;
-
     }
 
     // Sort based on arrival time
     for(int i=0; i<n; i++) {
-        for(int j=0; j< n-1; ) {
+        for(int j=0; j< n-1; j++) {
             if(r[j].at > r[j+1].at) {
                 struct RQ temp = r[j];
                 r[j]=r[j+1];
@@ -41,8 +39,95 @@ int main() {
 
     // SJF non preemptive
     count=0;
-    t=0;
+    t=0;   
+    avg_wt = 0;
+    avg_tat = 0;
+    avg_rest = 0; 
+    int smallest_bt = 0;
+    printf("\nSJF NON PREEMPTIVE SCHEDULING\nGANTT CHART\n|0|\t");
+    while(count < n) {
+        for(int j=0; j<n; j++) {
+            if((r[j].at <= t)&&(r[j].rt > 0)) {
+                smallest_bt = j;
+                break;
+            }
+        }
+        
+        for(int j=0; j<n; j++) {
+            if((r[j].at <= t) && (r[j].rt < r[smallest_bt].bt)&&(r[j].rt>0)) {
+                smallest_bt = j;
+            }
+        }
+        
+        r[smallest_bt].wt = t - r[smallest_bt].at;
+        r[smallest_bt].rest = t-r[smallest_bt].at;
+        r[smallest_bt].rt = 0;
+        t+=r[smallest_bt].bt;
+        printf("%s\t|%d|\t", r[smallest_bt].pid, t);
+        count ++;
+    }
+    
+    printf("\nCURRENT STATUS OF READY QUEUE\n");
+    printf("PID\tAT\tBT\tWT\tREST\n");
+    for(int i=0; i<n; i++) {
+        avg_wt += r[i].wt;
+        avg_tat+=(r[i].bt + r[i].wt);
+        avg_rest+=r[i].rest;
+        printf("%s\t%d\t%d\t%d\t%d\n",r[i].pid,r[i].at,r[i].bt,r[i].wt,r[i].rest);
+
+    }
+    
+    printf("AVERAGE WAITING TIME= %.2f\nAVERAGE TURNAROUND TIME = %.2f\nAVERAGE RESPONSE TIME = %.2f\nTHROUGHPUT=%.2f\n", (float)avg_wt/n, (float)avg_tat/n, (float)avg_rest/n, n/(float)(avg_tat-avg_wt));
+    
+    
+    //SRTF 
+    count = 0;
+    t = 0;
     avg_wt = 0;
     avg_tat = 0;
     avg_rest = 0;
+    smallest_bt = 0;
+    for(int j=0; j<n; j++) {
+        r[j].rt = r[j].bt;
+    }
+    
+    printf("\n\nSRTF SCHEDULING\nGANTT CHART\n|0|\t");
+    while(count < n) {
+        for(int j=0; j<n; j++) {
+            if((r[j].at <= t)&&(r[j].rt > 0)) {
+                smallest_bt = j;
+                break;
+            }
+        }
+        
+        for(int j=0; j<n; j++) {
+            if((r[j].at <= t) && (r[j].rt < r[smallest_bt].bt)&&(r[j].rt>0)) {
+                smallest_bt = j;
+            }
+        }
+        
+        r[smallest_bt].rt = r[smallest_bt].rt - 1;
+        if(r[smallest_bt].rt == (r[smallest_bt].bt - 1)) {
+            r[smallest_bt].wt = t - r[smallest_bt].at;
+        }
+        t++;
+        if(r[smallest_bt].rt == 0) {
+            r[smallest_bt].wt = t- r[smallest_bt].at - r[smallest_bt].bt;
+            count++;
+        }
+        printf("%s\t|%d|\t", r[smallest_bt].pid, t);
+        
+    }
+    
+    printf("\nCURRENT STATUS OF READY QUEUE\n");
+    printf("PID\tAT\tBT\tWT\tREST\n");
+    for(int i=0; i<n; i++) {
+        avg_wt += r[i].wt;
+        avg_tat+=(r[i].bt+r[i].wt);
+        avg_rest+=r[i].rest;
+        printf("%s\t%d\t%d\t%d\t%d\n",r[i].pid,r[i].at,r[i].bt,r[i].wt,r[i].rest);
+    }
+    
+     printf("AVERAGE WAITING TIME = %.2f\nAVERAGE TURNAROUND TIME = %.2f\nAVERAGE RESPONSE TIME = %.2f\nTHROUGHPUT = %.2f\n",(float)avg_wt/n,(float)avg_tat/n,(float)avg_rest/n,n/(float)(avg_tat-avg_wt));
+    
 }
