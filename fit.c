cf3cc4b3f96558012afe46a5ca3042c4beb92fc8
prathyushmadhu <prathyushmadhu@gmail.com>
2023-08-13 15:50:42 +0530

    changed the content of a new file

--- fit.c
+++ fit.c
@@ -38,6 +38,10 @@ int main() {
                 for(int i=0; i<n; i++) {
                     alloc[i] = 0;
                 }
+                printf("\n\n....Implementing first fit allocation....\n");
+                for(int i=0; i<p; i++) {
+                    printf("P%d\t",i);
+                }
                 printf("\n");
                 
                 // FIRST FIT
@@ -47,7 +51,7 @@ int main() {
                         if((memory[j] >= process[i]) && (alloc[j]==0)) {
                             alloc[j] = 1;
                             sum += (memory[j] - process[i]);
-                            printf("M%d\t",j)
+                            printf("M%d\t",j);
                             flag = 1;
                             break;
                         }
@@ -62,7 +66,89 @@ int main() {
                 printf("\ntotal Memory wastage due to internal fragmentation = %d\n",sum);
                 break;
                 
-                case 3:
+            case 3:
+                sum = 0;
+                
+                // ALLOCATION for each block
+                for(int i=0; i<n; i++) {
+                     alloc[i] = 0;
+                }
+                
+                printf("\n\n...Implementing best fit allocation....\n");
+                for(int i=0; i<p; i++) {
+                    printf("P%d\t",i);
+                }
+                printf("\n");
+                
+                // BEST FIT
+                for(int i=0; i<p; i++) {
+                    int flag=0;
+                    int smallest_value = 65535, smallest;
+                    for(int j=0; j<n; j++) {
+                        if((memory[j] >= process[i]) && (memory[j] < smallest_value) && (alloc[j] == 0)) {
+                            smallest = j;
+                            smallest_value = memory[j];
+                            flag = 1;
+                        }
+                    }
+                    
+                    if(!flag) {
+                        printf("\nCannot allocate all processes");
+                        break;
+                    }
+                    else {
+                        alloc[smallest] = 1;
+                        sum += (memory[smallest] - process[i]);
+                        printf("M%d\t",smallest);
+                    }
+                }
+                
+                printf("\ntotal Memory wastage due to internal fragmentation = %d\n",sum);
+                break;
+                
+            case 4:
+                sum = 0;
+                for(int i=0; i<n; i++) {
+                    alloc[i] = 0;
+                }
+                    
+                printf("\n\n...Implementing worst fit allocation....\n");
+                for(int i=0; i<p; i++) {
+                    printf("P%d\t",i);
+                }
+                printf("\n");
+                
+                // WORST FIT
+                for(int i=0; i<p; i++) {
+                    int flag=0;
+                    int largest_value = 0, largest;
+                    for(int j=0; j<n; j++) {
+                        if((memory[j] >= process[i]) && (memory[j] > largest_value) && (alloc[j] == 0)) {
+                            largest = j;
+                            largest_value = memory[largest];
+                            flag = 1;
+                        }
+                    }
+                    
+                    if(!flag) {
+                        printf("\nCannot allocate all process.");
+                        break;
+                    }
+                    else {
+                        alloc[largest] = 1;
+                        sum += (memory[largest] - process[i]);
+                        printf("M%d\t",largest);
+                    }
+                }
+                
+                printf("\ntotal Memory wastage due to internal fragmentation = %d\n",sum);
+                break;
+                
+            case 5:
+                printf("EXITING...\n");
+                break;
+            default:
+                printf("\nEnter valid choice\n");
         }
-    }
+    }while(c!=5);
 }
