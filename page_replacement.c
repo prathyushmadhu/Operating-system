6c8ca27982670899edaad69c312da4903e03409f
prathyushmadhu <prathyushmadhu@gmail.com>
2023-08-15 12:44:36 +0530

    changed a file content

--- page_replacement.c
+++ page_replacement.c
@@ -58,7 +58,66 @@ void fifo(int len, int ref[len], int size) {
     printf("Hit ratio: %d/%d\nMiss ratio: %d/%d\nPage faults: %d", hit, len, miss, len, miss);
 }
 
+// LRU
 void lru(int len, int ref[len], int size) {
     int mem[size];
     int hit=0,miss=0;
+    int flag=-1;
+    int entry[size];
+    int farthest=0;
+    int count = 0;
+    
+    for(int i=0; i<size; i++) {
+        mem[i] = -1;
+    }
+    
+    for(int i=0; i<len; i++) {
+        flag = -1;
+        
+        // Condition for hit
+        if(search(ref[i], size, mem) != -1) {
+            entry[search(ref[i], size, mem)] = i;
+            hit++;
+        }
+        else {
+            miss++;
+            
+            // If empty space in memory
+            for(int j=0; j<size; j++) {
+                if(mem[j] == -1) {
+                    mem[j] = ref[i];
+                    entry[j] = i;
+                    flag = 1;
+                    break;
+                }
+            }
+            
+            if(flag == -1) {
+                farthest = 0;
+                
+                // To get least recently (farthest) added index 
+                for(int j=0; j<size; j++) {
+                    if(entry[j] < entry[farthest]) {
+                        farthest = j;
+                    }
+                }
+                
+                mem[farthest] = ref[i];
+                entry[farthest] = i;
+                
+            }
+        }
+        
+        // Printing each state of memory buffer
+        for(int j=0; j<size; j++) {
+            printf("%d\t", mem[j]);
+        }
+        printf("\n");
+    }
+    printf("Hit ratio: %d/%d\nMiss ratio: %d/%d\nPage faults: %d", hit, len, miss, len, miss);
+}
+
+// Optimal
+void optimal(int len, int ref[len], int size) {
+    
 }
